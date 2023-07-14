1e87b1649a85f8289953e6a52edc7815609d0844
Matteo <menicacci@outlook.com>
2023-07-14 11:18:45 +0200

    graphic changes

--- assets/generate-images.js
+++ assets/generate-images.js
@@ -13,16 +13,16 @@ function generateImage(puzzle, cellSize, fontSize) {
     // Draw the background
     context.beginPath();
     context.rect(0, 0, imageSize, imageSize);
-    context.fillStyle = '#f0f0f0'; // Set the background color to light gray
+    context.fillStyle = '#f0f0f0';
     context.fill();
 
     // Define the font
     context.font = fontSize.toString() + 'px sans-serif';
-    context.fillStyle = 'black'; // Set the color of the numbers to black
+    context.fillStyle = 'black';
 
     for (let i = 0; i < puzzle.length; i++) {
-        let row = Math.floor(i / 4); // Row number (0-3)
-        let col = i % 4;                // Column number (0-3)
+        let row = Math.floor(i / 4);
+        let col = i % 4;
 
         // Calculate the coordinates of the cell rectangle
         let x1 = col * cellSize;
@@ -31,17 +31,18 @@ function generateImage(puzzle, cellSize, fontSize) {
         // Draw the cell rectangle
         context.beginPath();
         context.rect(x1, y1, cellSize, cellSize);
+        context.lineWidth = 1.5;
         context.strokeStyle = 'black';
         context.stroke();
 
-        const num = puzzle[i];
+        let num = puzzle[i];
         if (num !== 0) {
             // Write the number inside the cell
             let text = num.toString();
             let textWidth = context.measureText(text).width;
-            let textHeight = 20;
+            let textHeight = fontSize;
             let textX = x1 + (cellSize - textWidth) / 2;
-            let textY = y1 + (cellSize - textHeight) / 2 + textHeight;
+            let textY = y1 + (cellSize + textHeight) / 2;
             context.fillText(text, textX, textY);
         }
     }
--- assets/script.js
+++ assets/script.js
@@ -21,11 +21,19 @@ function initiateNavbar(configurations) {
 	for (let i = 0; i < configurations.length; i++) {
 		let image = new Image();
 		image.classList.add('nav-item', 'nav-link');
-		image.src = generateImage(configurations[i], 50, 20);
+		image.src = generateImage(configurations[i], 25, 15);
 		buttonContainer.appendChild(image);
 
+		// Add click event listener to toggle active class
 		image.addEventListener('click', function(event) {
 			event.preventDefault();
+			// Remove active class from all images
+			let images = document.querySelectorAll('.nav-item');
+			images.forEach(function(img) {
+				img.classList.remove('active');
+			});
+			// Add active class to clicked image
+			image.classList.add('active');
 			generateDigraph(i);
 		});
 	}
@@ -41,7 +49,7 @@ loadData()
 			dataset.push({puzzle: d[0], move_seq: d[1]})
 		});
 		initiateNavbar(data.map(v => v[0]));
-		generateDigraph(0);
+		// generateDigraph(0);
 	})
 	.catch((error) => {
 		console.log(error);
